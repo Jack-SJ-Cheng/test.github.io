a16f61827abb98cf6d04777e690f4a56a7d3a0ef
Jack Cheng <randomno@hotmail.com>
2021-06-14 22:53:16 +0800

    finish

--- all.js
+++ all.js
@@ -1,23 +1,28 @@
-
-
-const app = Vue.createApp({
-    //...
-});
-
 Object.keys(VeeValidateRules).forEach(rule => {
     if (rule !== 'default') {
         VeeValidate.defineRule(rule, VeeValidateRules[rule]);
     }
 });
-VeeValidateI18n.loadLocaleFromURL('./zh_TW.json');
 
+VeeValidateI18n.loadLocaleFromURL('./zh_TW.json');
 // Activate the locale
 VeeValidate.configure({
     generateMessage: VeeValidateI18n.localize('zh_TW'),
     validateOnInput: true, // 調整為輸入字元立即進行驗證
 });
+
+const app = Vue.createApp({
+    data() {
+        return {
+            user: {
+
+            }
+        }
+    }
+});
+
 app.component('VForm', VeeValidate.Form);
 app.component('VField', VeeValidate.Field);
 app.component('ErrorMessage', VeeValidate.ErrorMessage);
 
-app.mount('#app');
+app.mount('#app')
